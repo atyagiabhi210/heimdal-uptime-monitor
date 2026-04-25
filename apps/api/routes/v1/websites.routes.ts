807be7aaf3a80ffdf6db1886b74711c2caece7d0
atyagiabhi210 <atyagiabhi210@gmail.com>
2026-04-25 19:37:20 +0530

    add basic crud functionality

--- apps/api/routes/v1/websites.routes.ts
+++ apps/api/routes/v1/websites.routes.ts
@@ -1,9 +1,37 @@
 import { Router } from "express";
+import { prisma } from "store/client";
 
 const router = Router();
 
-router.post("/website", (req, res) => {});
+router.post("/website", async (req, res) => {
+  if (!req.body.url) {
+    return res.status(411).json({
+      status: "error",
+      message: "URL is required",
+    });
+  }
+  const website = await prisma.website.create({
+    data: {
+      url: req.body.url,
+      timeAdded: new Date(),
+    },
+  });
+  res.json({
+    status: "ok",
+    data: {
+      websiteId: website.id,
+    },
+  });
+});
 
-router.get("/status/:websiteId", (req, res) => {});
+router.get("/status/:websiteId", (req, res) => {
+  res.json({
+    status: "ok",
+    data: {
+      websiteId: req.params.websiteId,
+      status: "ok",
+    },
+  });
+});
 
 export default router;
